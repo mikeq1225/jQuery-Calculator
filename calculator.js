7692dba1b4f8d2570378fd0b6a1abc6e4cc61d17
Michael Quarne <mikedq1225@gmail.com>
2020-02-05 19:27:57 -0800

    tried adventure mode

--- calculator.js
+++ calculator.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  $("#add").on("click", function(e) {
+  var display = ""
+  var number1 = 0
+  $("#input").val(display)
+  $("button").on("click", function(e) {
     e.preventDefault()
-    var add = Number($("#num1").val()) + Number($("#num2").val())
-    $("#answer").val(add)
+    $("button").removeClass("pushed")
+    $(this).addClass("pushed")
+    if ($(this).text() == "C") {
+      display = ""
+    }
+    $("#input").val((display += $(this).val()))
+    console.log(display)
+    if ($(this).attr("class") == "operators") {
+      Number(display) = Number(number1)
+      display = ""
+      console.log(number1)
+    }
   })
 })
